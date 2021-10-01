a30f9f506555338db48e7be8d4d0b5ef3ee78d57
Le Vivilet <levivilet@pm.me>
2021-10-01 18:29:08 +0200

    add test stub for moving out folders memory usage

--- test/memory-usage.test.js
+++ test/memory-usage.test.js
@@ -1,10 +1,8 @@
-import { mkdir, rm, writeFile } from "fs/promises";
 import { spawn } from "child_process";
-import { mkdtemp } from "fs/promises";
+import { mkdir, mkdtemp, rename, rm, writeFile } from "fs/promises";
 import { tmpdir } from "os";
 import { join } from "path";
 import pidusage from "pidusage";
-import { jest } from "@jest/globals";
 import waitForExpect from "wait-for-expect";
 
 const getStats = async (pid) => {
@@ -117,6 +115,72 @@ test("memory should not grow when adding and removing folders", async () => {
   const middleStats4 = await getStats(watcher.pid);
   for (let i = 0; i < 5_000; i++) {
     await rm(`${tmpDir}/4-${i}`, { recursive: true });
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(40_000);
+  });
+  const finalStats = await getStats(watcher.pid);
+  expect(middleStats1.memory).toBeGreaterThan(initialStats.memory);
+  expect(middleStats2.memory).toBe(middleStats3.memory);
+  expect(middleStats3.memory).toBe(middleStats4.memory);
+  expect(middleStats4.memory).toBe(finalStats.memory);
+  watcher.dispose();
+}, 40_000);
+
+test.skip("memory should not grow when moving out folders", async () => {
+  const tmpDir = await getTmpDir();
+  const tmpDir2 = await getTmpDir();
+  const watcher = await createWatcher([tmpDir]);
+  const initialStats = await getStats(watcher.pid);
+  for (let i = 0; i < 5_000; i++) {
+    await mkdir(`${tmpDir}/1-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(5_000);
+  });
+  const middleStats1 = await getStats(watcher.pid);
+  // await setTimeout(100);
+  for (let i = 0; i < 5_000; i++) {
+    await rename(`${tmpDir}/1-${i}`, `${tmpDir2}/1-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(10_000);
+  });
+  for (let i = 0; i < 5_000; i++) {
+    await mkdir(`${tmpDir}/2-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(15_000);
+  });
+  const middleStats2 = await getStats(watcher.pid);
+  for (let i = 0; i < 5_000; i++) {
+    await rename(`${tmpDir}/2-${i}`, `${tmpDir2}/2-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(20_000);
+  });
+  for (let i = 0; i < 5_000; i++) {
+    await mkdir(`${tmpDir}/3-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(25_000);
+  });
+  const middleStats3 = await getStats(watcher.pid);
+  for (let i = 0; i < 5_000; i++) {
+    await rename(`${tmpDir}/3-${i}`, `${tmpDir2}/3-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(30_000);
+  });
+  for (let i = 0; i < 5_000; i++) {
+    await mkdir(`${tmpDir}/4-${i}`);
+  }
+  await waitForExpect(() => {
+    expect(watcher.eventCount).toBe(35_000);
+  });
+  const middleStats4 = await getStats(watcher.pid);
+  for (let i = 0; i < 5_000; i++) {
+    await rename(`${tmpDir}/4-${i}`, `${tmpDir2}/4-${i}`);
   }
   await waitForExpect(() => {
     expect(watcher.eventCount).toBe(40_000);
