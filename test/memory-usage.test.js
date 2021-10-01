2edce0b9fde666b914e4b67216e0dad228ffbf84
Le Vivilet <levivilet@pm.me>
2021-10-01 17:43:16 +0200

    add back one memory usage test

--- test/memory-usage.test.js
+++ test/memory-usage.test.js
@@ -50,7 +50,7 @@ export const createWatcher = async (args = [], options = {}) => {
   };
 };
 
-test.skip("memory should stay the same when adding files", async () => {
+test("memory should stay the same when adding files", async () => {
   const tmpDir = await getTmpDir();
   const watcher = await createWatcher([tmpDir]);
   const initialStats = await getStats(watcher.pid);
